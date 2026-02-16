1f1b152dab3978f698d884837ddcc65df242c6f3
Abdus Sami <safwan.australia@gmail.com>
2026-02-16 21:17:37 +1100

    Pink Morsel vis uploaded

--- tasks/Task2.py
+++ tasks/Task2.py
@@ -2,17 +2,15 @@ import csv
 
 data_files = ['daily_sales_data_0.csv','daily_sales_data_1.csv','daily_sales_data_2.csv']
 
-with open('full_daily_sales_data.csv', mode='w', newline='') as csv_file_full:
+with open('../data/full_daily_sales_data.csv', mode='w', newline='') as csv_file_full:
   fieldnames = ['sales','date','region']
   csv_writer = csv.DictWriter(csv_file_full, fieldnames)
   csv_writer.writeheader()
 
   for file in data_files:
-    with open(file, mode='r') as csv_file:
+    with open("../data/"+file, mode='r') as csv_file:
       csv_reader = csv.DictReader(csv_file)
       for row in csv_reader:
         if row['product'] == 'pink morsel':
           sales = float(row['price'][1:]) * int(row['quantity'])
           csv_writer.writerow({'sales': sales, 'date': row['date'], 'region': row['region']})
-
-  
---
+++ tasks/Task3.py
@@ -0,0 +1,17 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash()
+
+df = pd.read_csv("../data/full_daily_sales_data.csv")
+
+fig = px.line(df, x='date', y='sales', title='Sales Over Time')
+
+app.layout = html.Div([
+    html.H1("Pink Morsel Visualisation"),
+    dcc.Graph(figure=fig)
+])
+
+if __name__ == "__main__":
+    app.run(debug=True)
