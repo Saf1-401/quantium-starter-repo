959a28a25deea9f44f31c2173012e88bc9833831
Abdus Sami <safwan.australia@gmail.com>
2026-02-16 17:56:45 +1100

    Full data filtered by Pink Morsel

--- data/script.py
+++ data/script.py
@@ -3,7 +3,7 @@ import csv
 data_files = ['daily_sales_data_0.csv','daily_sales_data_1.csv','daily_sales_data_2.csv']
 
 with open('full_daily_sales_data.csv', mode='w', newline='') as csv_file_full:
-  fieldnames = ['product','price','quantity','date','region','sales']
+  fieldnames = ['sales','date','region']
   csv_writer = csv.DictWriter(csv_file_full, fieldnames)
   csv_writer.writeheader()
 
@@ -11,7 +11,8 @@ with open('full_daily_sales_data.csv', mode='w', newline='') as csv_file_full:
     with open(file, mode='r') as csv_file:
       csv_reader = csv.DictReader(csv_file)
       for row in csv_reader:
-        row['sales'] = float(row['price'][1:]) * int(row['quantity'])
-        csv_writer.writerow(row)
+        if row['product'] == 'pink morsel':
+          sales = float(row['price'][1:]) * int(row['quantity'])
+          csv_writer.writerow({'sales': sales, 'date': row['date'], 'region': row['region']})
 
   
